eab4cf2b703dd464a5cb08de2b148d2679c2bde6
Dayo Adebanjo <dahyor@outlook.com>
2021-09-11 19:19:43 +0100

    Added Nav and Nav Animations

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -1,20 +1,20 @@
-import {useRef, useEffect} from 'react';
-import styles from '@/uiStyles/Background.module.scss';
+// import {useRef, useEffect} from 'react';
+// import styles from '@/uiStyles/Background.module.scss';
 // import { init, animate } from '@/helpers/backgroundAnimation'; 
 
 const Background: React.FC = ({children}) => {
     // const animatedBackground = useRef<HTMLDivElement>(null)
-    useEffect(() => {
+    // useEffect(() => {
         // const element = animatedBackground
         // init(element)
         // animate()
         // return () => {
         //     element.current && element.current.removeChild(element.current.childNodes[1])
         // }
-    }, [])
+    // }, [])
 
     return (
-        <div className={styles.background} >
+        <div>
             {children}
         </div>
     )
