f5929d7afe88ba257666a4aa4697cb06514e0bc2
Dayo Adebanjo <63509119+dahyorr@users.noreply.github.com>
2022-03-01 09:01:00 +0100

    temp

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -4,8 +4,7 @@ const Landing: React.FC = () => {
     return (
         <div className={styles.Landing}>
             <div className={styles.landingContent}>
-                <h1>{`Hi There!, I'm Dayo`}</h1>
-                {/* <p>I&apos;m a pationate FullStack Developer </p> */}
+                
             </div>
             <div className={styles.landingDesign}>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,29 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Head from 'next/head'
-import Link from 'next/link'
+// import Link from 'next/link'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import LinkedButton from '@/components/ui/LinkedButton'
-import AnimationWrapper from '@/components/AnimationWrapper'
-import Landing from '@/components/Landing'
+// import LinkedButton from '@/components/ui/LinkedButton'
+// import AnimationWrapper from '@/components/AnimationWrapper'
+// import Landing from '@/components/Landing'
 
 const Home: NextPage = () => {
   return (
-    <AnimationWrapper>
-      <div className={styles.home}>
-        <Head>
-          <title>Dayo Adebanjo | Home</title>
-        </Head>
-          <Landing/>
+    <>
+      <Head>
+            <title>Dayo Adebanjo | Home</title>
+      </Head>
+      <div className={styles.Home}>
+        
+          {/* <Landing/> */}
         {/* <LinkedButton direction="next" path="/about"> About</LinkedButton> */}
+        {/* <div><Image src='/wave.svg' alt='Background' width='100vw' height='100vh'/></div> */}
+        <div className={styles.content}>
+          <h2>Hi I&apos;m Dayo, A FullStack Web Developer</h2>
+        </div>
       </div>
-    </AnimationWrapper>
+    </>
   )
 }
 
