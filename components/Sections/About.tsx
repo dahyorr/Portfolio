60b00e01c2acd93d43651434a85aa73e18da8192
Dayo Adebanjo <dahyor@outlook.com>
2022-04-16 10:09:13 +0100

    Added Socials

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -2,13 +2,31 @@ import Box from '@mui/material/Box'
 import PageSection from '../PageSection'
 
 const About: React.FC = () => {
-    return (
-        <PageSection title="about" >
-            <Box width="100%" height="100%" >
-                About
-            </Box>
-        </PageSection>
-    )
+  return (
+    <PageSection 
+      title="about" 
+      sx={{position: 'relative'}}
+    >
+      <Box 
+        id="pre-about"
+        sx={{
+          position: 'absolute',
+          zIndex: -1,
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100vh',
+          overflow: 'hidden'
+        }}
+      >
+        
+      </Box>
+
+      <Box width="100%" height="100%" >
+        About
+      </Box>
+    </PageSection>
+  )
 }
 
 export default About
--- components/Sections/Landing.tsx
+++ components/Sections/Landing.tsx
@@ -10,13 +10,14 @@ import particlesOptions from '../../helpers/particles.json'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
-import { styled } from '@mui/system';
-import {GrSend} from 'react-icons/gr'
+import Link from '@mui/material/Link'
+import IconButton from '@mui/material/IconButton'
+// import { styled } from '@mui/system';
 import {BiMailSend} from 'react-icons/bi'
-
-const SolidBox = styled(Box)<BoxProps>(({theme}) => ({
-  backgroundColor: theme.palette.primary.main
-}))
+import {AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillMail} from 'react-icons/ai'
+// const SolidBox = styled(Box)<BoxProps>(({theme}) => ({
+//   backgroundColor: theme.palette.primary.main
+// }))
 
 const Landing: React.FC = () => {
 
@@ -30,6 +31,7 @@ const Landing: React.FC = () => {
             bgcolor="transparent"
             sx={{
                 position: 'relative',
+                pl: 3,
                 '& #landing-background': {
                     height: '100%',
                     width: '100%',
@@ -42,6 +44,54 @@ const Landing: React.FC = () => {
                 }
             }}
         >
+          <Stack
+            id="socials"
+            spacing={1.5}
+            sx={{
+              position: 'absolute',
+              height: 'fit-content',
+              minHeight: 20,
+              // bgcolor: 'primary.main',
+              left: 0,
+              top: '35%',
+            }}
+          >
+            <IconButton 
+              component={Link} 
+              href="https://www.github.com/dahyorr" 
+              target="_blank"
+              aria-label="github"
+            >
+              <AiFillGithub color="white"/>
+            </IconButton>
+            <IconButton 
+              component={Link} 
+              href="https://twitter.com/dah_yor" 
+              target="_blank"
+              aria-label="twitter"
+            >
+              <AiOutlineTwitter color="white"/>
+            </IconButton>
+
+            <IconButton 
+              component={Link} 
+              href="https://www.linkedin.com/in/dayo-adebanjo-4a5381126/" 
+              target="_blank"
+              aria-label="linkedin"
+            >
+              <AiFillLinkedin color="white"/>
+            </IconButton>
+
+            <IconButton 
+              component={Link} 
+              href="mailto:hello@dayo.dev" 
+              target="_blank"
+              aria-label="mail"
+            >
+              <AiFillMail color="white"/>
+            </IconButton>
+          </Stack>
+
             <Particles 
                 id="landing-background" 
                 options={particlesOptions as ISourceOptions}
@@ -67,15 +117,15 @@ const Landing: React.FC = () => {
                 </Typography>
                 <Typography 
                   color="primary"
-                  variant="h1"
-                  fontSize="8rem"
+                  // variant="h1"
+                  fontSize="7.5rem"
                   fontWeight={700}
                 >
                   Dayo
                 </Typography>
                 <Typography 
                   color="white"
-                  fontSize="3.6rem"
+                  fontSize="3.4rem"
                   fontWeight={700}
                 >
                   Full-Stack Web Developer
