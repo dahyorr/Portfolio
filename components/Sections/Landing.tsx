01b504e1e1dee9c8295b05e5f533664ab4c3d484
Dayo Adebanjo <dahyor@outlook.com>
2023-05-14 16:50:51 +0100

    fix landing buttons

--- components/Sections/Landing.tsx
+++ components/Sections/Landing.tsx
@@ -1,6 +1,6 @@
-import {useCallback} from 'react'
-import NextLink  from 'next/link'
-import Box,{BoxProps} from '@mui/material/Box'
+import { useCallback } from 'react'
+import NextLink from 'next/link'
+import Box, { BoxProps } from '@mui/material/Box'
 import PageSection from '../PageSection'
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
@@ -13,9 +13,9 @@ import Button from '@mui/material/Button'
 import Link from '@mui/material/Link'
 import IconButton from '@mui/material/IconButton'
 import useMediaQuery from '@mui/material/useMediaQuery';
-import {useTheme} from '@mui/material/styles';
-import {BiMailSend} from 'react-icons/bi'
-import {AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillMail} from 'react-icons/ai'
+import { useTheme } from '@mui/material/styles';
+import { BiMailSend } from 'react-icons/bi'
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillMail } from 'react-icons/ai'
 import socialLinks from 'helpers/socials.json'
 
 const Landing: React.FC = () => {
@@ -26,142 +26,144 @@ const Landing: React.FC = () => {
     loadFull(engine);
   }, []);
 
-    return (
-        <PageSection 
-            title="home" 
-            bgcolor="transparent"
-            viewportHeight
-            sx={{
-                position: 'relative',
-                '& #landing-background': {
-                    height: '100%',
-                    width: '100%',
-                    bgcolor: 'secondary.main',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    zIndex: -1,
-                    overflow: 'hidden'
-                }
-            }}
+  return (
+    <PageSection
+      title="home"
+      bgcolor="transparent"
+      viewportHeight
+      sx={{
+        position: 'relative',
+        '& #landing-background': {
+          height: '100%',
+          width: '100%',
+          bgcolor: 'secondary.main',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          overflow: 'hidden'
+        }
+      }}
+    >
+      {!isLargeScreen && (<Stack
+        id="socials"
+        spacing={1.5}
+        sx={{
+          position: 'absolute',
+          height: 'fit-content',
+          minHeight: 20,
+          // bgcolor: 'primary.main',
+          left: 0,
+          top: '35%',
+        }}
+      >
+        <IconButton
+          component={Link}
+          href={socialLinks.github}
+          target="_blank"
+          aria-label="github"
         >
-          {!isLargeScreen && (<Stack
-            id="socials"
-            spacing={1.5}
-            sx={{
-              position: 'absolute',
-              height: 'fit-content',
-              minHeight: 20,
-              // bgcolor: 'primary.main',
-              left: 0,
-              top: '35%',
-            }}
+          <AiFillGithub color="white" />
+        </IconButton>
+        <IconButton
+          component={Link}
+          href={socialLinks.twitter}
+          target="_blank"
+          aria-label="twitter"
+        >
+          <AiOutlineTwitter color="white" />
+        </IconButton>
+
+        <IconButton
+          component={Link}
+          href={socialLinks.linkedin}
+          target="_blank"
+          aria-label="linkedin"
+        >
+          <AiFillLinkedin color="white" />
+        </IconButton>
+
+        <IconButton
+          component={Link}
+          href={socialLinks.mailTo}
+          target="_blank"
+          aria-label="mail"
+        >
+          <AiFillMail color="white" />
+        </IconButton>
+      </Stack>)}
+
+      <Particles
+        id="landing-background"
+        options={particlesOptions as ISourceOptions}
+        init={particlesInit}
+        width="100%"
+        height="100%"
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: '100%',
+          // bgcolor: 'red'
+        }}
+      >
+        <Stack spacing={0}>
+          <Typography
+            color="white"
+            fontSize="1.6rem"
+            fontWeight={700}
           >
-            <IconButton 
-              component={Link} 
-              href={socialLinks.github}
-              target="_blank"
-              aria-label="github"
-            >
-              <AiFillGithub color="white"/>
-            </IconButton>
-            <IconButton 
-              component={Link} 
-              href={socialLinks.twitter}
-              target="_blank"
-              aria-label="twitter"
-            >
-              <AiOutlineTwitter color="white"/>
-            </IconButton>
+            Hi, I&apos;m
+          </Typography>
+          <Typography
+            color="primary"
+            // variant="h1"
+            fontSize="7.5rem"
+            fontWeight={700}
+          >
+            Dayo
+          </Typography>
+          <Typography
+            color="white"
+            fontSize="3.4rem"
+            fontWeight={700}
+          >
+            Full-Stack Web Developer
+          </Typography>
 
-            <IconButton 
-              component={Link} 
-              href={socialLinks.linkedin}
-              target="_blank"
-              aria-label="linkedin"
-            >
-              <AiFillLinkedin color="white"/>
-            </IconButton>
+          <Stack sx={{ flexDirection: 'row', mt: 4 }}>
+            <NextLink href="#contact" passHref>
+              <Button
+                variant="contained"
+                size="large"
+                endIcon={<BiMailSend color="#fff" />}
+                sx={{ py: 2, px: 4, color: 'white', mr: 2 }}
+              >
+                Contact Me
+              </Button>
+            </NextLink>
 
-            <IconButton 
-              component={Link} 
-              href={socialLinks.mailTo}
-              target="_blank"
-              aria-label="mail"
-            >
-              <AiFillMail color="white"/>
-            </IconButton>
-          </Stack>)}
-
-            <Particles 
-                id="landing-background" 
-                options={particlesOptions as ISourceOptions}
-                init={particlesInit}
-                width="100%"
-                height="100%"
-            />
-            <Box 
+            <NextLink href="#about" passHref>
+              <Button
+                variant="outlined"
+                size="large"
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  height: '100%',
-                  // bgcolor: 'red'
+                  color: 'white',
+                  py: 2,
+                  px: 4,
                 }}
-            > 
-              <Stack spacing={0}>
-                <Typography 
-                  color="white"
-                  fontSize="1.6rem"
-                  fontWeight={700}
-                >
-                  Hi, I&apos;m
-                </Typography>
-                <Typography 
-                  color="primary"
-                  // variant="h1"
-                  fontSize="7.5rem"
-                  fontWeight={700}
-                >
-                  Dayo
-                </Typography>
-                <Typography 
-                  color="white"
-                  fontSize="3.4rem"
-                  fontWeight={700}
-                >
-                  Full-Stack Web Developer
-                </Typography>
-
-                <Stack sx={{flexDirection: 'row',  mt: 4 }}>
-                  <NextLink href="#contact" passHref>
-                    <Button 
-                      variant="contained" 
-                      size="large"
-                      endIcon={<BiMailSend color="#fff"/>}
-                      sx={{py: 2, px: 4, color: 'white', mr: 2}}
-                    >
-                      Contact Me
-                    </Button>
-                  </NextLink>
-
-                  <NextLink href="#about" passHref>
-                    <Button 
-                      variant="outlined" 
-                      size="large"
-                      sx={{
-                        color: 'white'
-                      }}
-                    >
-                      About me
-                    </Button>
-                  </NextLink>
-                </Stack>
-              </Stack>
+              >
+                About me
+              </Button>
+            </NextLink>
+          </Stack>
+        </Stack>
 
 
-            </Box>
-        </PageSection>        
-    )
+      </Box>
+    </PageSection>
+  )
 }
 
 export default Landing
