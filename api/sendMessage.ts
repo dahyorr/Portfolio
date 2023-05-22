ed47cb9a00761da35c173017c655521be66f55ff
Dayo Adebanjo <dahyor@outlook.com>
2023-05-22 19:44:24 +0100

    minor fix

--- api/sendMessage.ts
+++ api/sendMessage.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {MessageRequestBody} from 'typings'
+import { MessageRequestBody } from 'typings'
 
 export const sendMessage = async (body: MessageRequestBody) => {
   return axios.post(process.env.NEXT_PUBLIC_FORMSPREE_URL || '', body)
--- components/Sections/Contact.tsx
+++ components/Sections/Contact.tsx
@@ -7,23 +7,23 @@ import Grid from '@mui/material/Grid'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Paper from '@mui/material/Paper'
 import PageSection from '../PageSection'
-import {useFormik} from 'formik'
+import { useFormik } from 'formik'
 import * as yup from 'yup'
-import {sendMessage} from 'api/sendMessage'
+import { sendMessage } from 'api/sendMessage'
 import { MessageRequestBody } from 'typings'
-import {useState} from 'react'
-import {useSnackbar} from 'notistack'
-import {FiSend} from 'react-icons/fi'
+import { useState } from 'react'
+import { useSnackbar } from 'notistack'
+import { FiSend } from 'react-icons/fi'
 
 const Contact: React.FC = () => {
     const [loading, setLoading] = useState(false)
-    const {enqueueSnackbar} = useSnackbar()
-    
+    const { enqueueSnackbar } = useSnackbar()
+
     const validationSchema = yup.object().shape({
-        email: yup.string().email().required(),
-        name: yup.string().required(),
-        subject: yup.string(),
-        message: yup.string().required(),
+        email: yup.string().email().required().label("Email"),
+        name: yup.string().required().label("Name"),
+        subject: yup.string().label("Subject"),
+        message: yup.string().required().label("Message"),
     })
 
     const initialValues = {
@@ -40,7 +40,7 @@ const Contact: React.FC = () => {
                 enqueueSnackbar("Message sent successfully", {
                     variant: 'success',
                 })
-                formik.resetForm()
+                // formik.resetForm()
             })
             .catch(err => {
                 console.log(err)
@@ -79,84 +79,84 @@ const Contact: React.FC = () => {
                     elevation={6}
                 >
                     <Typography variant="h3" color="primary" mb={2}>Get In Touch</Typography>
-                    <Container 
-                        maxWidth="md" 
+                    <Container
+                        maxWidth="md"
                         component="form"
-                        onSubmit={formik.handleSubmit} 
+                        onSubmit={formik.handleSubmit}
                         sx={{
                             display: 'flex',
-                            flexDirection:'column',
+                            flexDirection: 'column',
                             alignItems: 'center',
                         }}
                     >
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        name='name'
-                                        label='Name'
-                                        type="text"
-                                        variant="filled"
-                                        value={formik.values.name}
-                                        error={Boolean(formik.errors.name)}
-                                        helperText={formik.errors.name || ' '}
-                                        fullWidth
-                                        onChange={formik.handleChange}
-                                        // color="white"
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        name='email'
-                                        label='Email'
-                                        type="email"
-                                        variant="filled"
-                                        error={Boolean(formik.errors.email)}
-                                        helperText={formik.errors.email || ' '}
-                                        fullWidth
-                                        value={formik.values.email}
-                                        onChange={formik.handleChange}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        name='subject'
-                                        label='Subject'
-                                        type="text"
-                                        variant="filled"
-                                        fullWidth
-                                        error={Boolean(formik.errors.subject)}
-                                        helperText={formik.errors.subject || ' '}
-                                        value={formik.values.subject}
-                                        onChange={formik.handleChange}
-                                    />
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <TextField
-                                        multiline
-                                        minRows={3}
-                                        maxRows={4}
-                                        name='message'
-                                        label='Message'
-                                        type="text"
-                                        variant="filled"
-                                        fullWidth
-                                        error={Boolean(formik.errors.message)}
-                                        helperText={formik.errors.message || ' '}
-                                        value={formik.values.message}
-                                        onChange={formik.handleChange}
-                                    />
-                                </Grid>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    name='name'
+                                    label='Name'
+                                    type="text"
+                                    variant="filled"
+                                    value={formik.values.name}
+                                    error={Boolean(formik.errors.name)}
+                                    helperText={formik.errors.name || ' '}
+                                    fullWidth
+                                    onChange={formik.handleChange}
+                                // color="white"
+                                />
                             </Grid>
-                        
-                        <LoadingButton 
-                            variant="outlined" 
-                            type="submit" 
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    name='email'
+                                    label='Email'
+                                    type="email"
+                                    variant="filled"
+                                    error={Boolean(formik.errors.email)}
+                                    helperText={formik.errors.email || ' '}
+                                    fullWidth
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name='subject'
+                                    label='Subject'
+                                    type="text"
+                                    variant="filled"
+                                    fullWidth
+                                    error={Boolean(formik.errors.subject)}
+                                    helperText={formik.errors.subject || ' '}
+                                    value={formik.values.subject}
+                                    onChange={formik.handleChange}
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    multiline
+                                    minRows={3}
+                                    maxRows={4}
+                                    name='message'
+                                    label='Message'
+                                    type="text"
+                                    variant="filled"
+                                    fullWidth
+                                    error={Boolean(formik.errors.message)}
+                                    helperText={formik.errors.message || ' '}
+                                    value={formik.values.message}
+                                    onChange={formik.handleChange}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <LoadingButton
+                            variant="outlined"
+                            type="submit"
                             size="large"
                             sx={{
                                 mx: 'auto',
                                 mt: 4,
-                                
+
                             }}
                             endIcon={<FiSend />}
                             loading={loading}
